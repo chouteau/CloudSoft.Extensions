4bef115a5391e340581ba490ab901841f1349e93
Marc Chouteau <chouteau@gmail.com>
2013-02-18 22:19:56 +0100

    Change AssemblyName

--- CloudSoft.Extensions/Properties/AssemblyInfo.cs
+++ CloudSoft.Extensions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CloudSoft.Common")]
+[assembly: AssemblyTitle("CloudSoft.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CloudSoft.Common")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("CloudSoft.Extensions")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
